6690727931da49fb807fb91d68ca38da9dd7085c
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-28 23:40:41 +0530

    Initialize Redux project with Vite, React, and Redux Toolkit
    - Created package.json with necessary dependencies and scripts.
    - Added Vite logo SVG file.
    - Implemented main App component and Todo component.
    - Set up Redux store with todoSlice for managing todo state.
    - Created todoSlice with actions for adding, deleting, updating, and completing todos.
    - Configured main entry point with Redux Provider.
    - Added Vite configuration file for project setup.
    - Included empty CSS file for styling.

---
+++ Redux-1/src/features/todoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState: {
+    list: [],
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      state.list.push(action.payload);
+    },
+    deleteTodo: (state, action) => {
+      state.list = state.list.filter((item) => item.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      state.list.forEach((todo) => {
+        if (todo.id === action.payload.id) {
+          todo.item = action.payload.item;
+        }
+      });
+    },
+    completeTodo: (state, action) => {
+      state.list.forEach((todo) => {
+        if (todo.id === action.payload) {
+          todo.completed = !todo.completed;
+        }
+      });
+    },
+  },
+});
+
+export const { addTodo, deleteTodo, updateTodo, completeTodo } =
+  todoSlice.actions;
+
+export default todoSlice.reducer;
