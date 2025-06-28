a011a70e4e19cf6183864c427ca5ec0990c82f25
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-29 00:16:41 +0530

    Implement Todo component with add, update, complete, and delete functionalities

--- Redux-1/src/components/Todo.jsx
+++ Redux-1/src/components/Todo.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addTodo,
+  completeTodo,
+  deleteTodo,
+  updateTodo,
+} from "../features/todoSlice.js";
 
 const Todo = () => {
-  return <div>Todo</div>;
+  const [todo, setTodo] = useState("");
+  const [editText, setEditText] = useState("");
+  const [editId, setEditId] = useState(null);
+
+  const dispatch = useDispatch();
+  const todoState = useSelector((state) => state.todo.list);
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId !== null) {
+      dispatch(updateTodo({ id: editId, text: editText }));
+      setEditId(null);
+      setEditText("");
+    } else {
+      dispatch(
+        addTodo({
+          id: Date.now(),
+          text: todo,
+          completed: false,
+        })
+      );
+      setTodo("");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handlerSubmit}>
+        <h1>Todo App</h1>
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          value={editId !== null ? editText : todo}
+          onChange={(e) =>
+            editId !== null
+              ? setEditText(e.target.value)
+              : setTodo(e.target.value)
+          }
+        />
+        <button type="submit">{editId !== null ? "Update" : "Add"}</button>
+      </form>
+
+      <div>
+        {todoState.map((todo) => (
+          <div key={todo.id}>
+            <h3
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.text}
+            </h3>
+
+            <button onClick={() => dispatch(completeTodo(todo.id))}>
+              {todo.completed ? "Undo" : "Complete"}
+            </button>
+
+            <button onClick={() => dispatch(deleteTodo(todo.id))}>
+              Delete
+            </button>
+
+            <button
+              onClick={() => {
+                setEditId(todo.id);
+                setEditText(todo.text);
+              }}
+            >
+              Edit
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Todo;
