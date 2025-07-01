3f81f841bc3a9a67b3052f75fbfcb94df62ec46d
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-07-01 12:49:24 +0530

    feat: implement Todo functionality with add and delete features using Redux

--- Redux-3(P)(CRUD)/src/App.jsx
+++ Redux-3(P)(CRUD)/src/App.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, deleteTodo } from "./features/TodoSlice.js";
 
 const App = () => {
-  return <div>App</div>;
+  const [todo, setTodo] = useState("");
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todo.list);
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(todo));
+    setTodo("");
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Todo App with Add and Delete</h1>
+        <form onSubmit={handlerSubmit}>
+          <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button>Add</button>
+        </form>
+        <ul>
+          {todos.map((todo) => (
+            <div key={todo.id}>
+              <li>{todo.text}</li>
+              <button onClick={() => dispatch(deleteTodo(todo.id))}>
+                Delete
+              </button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default App;
